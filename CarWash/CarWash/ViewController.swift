1c3171260d583b4e94f534b869c05c3ababfb949
Yana <diangeloq@gmail.com>
2021-10-06 14:48:41 +0300

    project created

---
+++ CarWash/CarWash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarWash
+//
+//  Created by IOS on 06.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
